9ecff1e7b5987fe7d2dec29918a79f5529ec5e22
Monty3141 <juanpablomontoyae@gmail.com>
2019-01-21 20:14:24 -0700

    api

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return '<h1>Deployed to heroku</h1>'
+@app.route("/api")
+def api():
+    return 'this is the api route'
 if __name__ == '__main__':
     app.run(port = 9000)
