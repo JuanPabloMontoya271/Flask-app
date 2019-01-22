0f159d0fca9d87d0299715711ba093ba91fb77dc
Monty3141 <juanpablomontoyae@gmail.com>
2019-01-21 19:51:14 -0700

    commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 def index():
     return '<h1>Deployed to heroku</h1>'
-#if __name__ == '__main__':
-#    app.run(port = 9000, debug =True)
+if __name__ == '__main__':
+    app.run(port = 9000, debug =True)
